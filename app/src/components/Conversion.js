ac6635eb4896efcd009876efec7bf070b84d63f6
Emanuelle Neves <43800901+May199@users.noreply.github.com>
2021-06-08 15:32:44 -0300

    euro add

--- app/src/components/Conversion.js
+++ app/src/components/Conversion.js
@@ -31,7 +31,9 @@ export default class Conversion extends Component {
         let valor = parseFloat(this.state.num);
         let converted = 0.0;
         let dolar = 5.05;
-    
+        let euro = 6.14;
+
+        // Conversão de real para dolar e dolar para real
         if(this.state.moedaOf == 'real' && this.state.moedaTo == 'dolar'){
             converted = valor / dolar;
             document.getElementById('resultado').innerHTML = converted.toFixed(2) + ' Dólar americano';
@@ -39,6 +41,26 @@ export default class Conversion extends Component {
         if(this.state.moedaOf == 'dolar' && this.state.moedaTo == 'real'){
             converted = valor * dolar;
             document.getElementById('resultado').innerHTML = converted.toFixed(2) + ' Real brasileiro';
+        }
+
+        // Conversão de real para euro e euro para real
+        if(this.state.moedaOf == 'real' && this.state.moedaTo == 'euro'){
+            converted = valor / euro;
+            document.getElementById('resultado').innerHTML = converted.toFixed(2) + ' Euro';
+        }
+        if(this.state.moedaOf == 'euro' && this.state.moedaTo == 'real'){
+            converted = valor * euro;
+            document.getElementById('resultado').innerHTML = converted.toFixed(2) + ' Real brasileiro';
+        }
+
+        // Conversão de dolar para euro e euro para dolar
+        if(this.state.moedaOf == 'dolar' && this.state.moedaTo == 'euro'){
+            converted = valor / euro;
+            document.getElementById('resultado').innerHTML = converted.toFixed(2) + ' Euro';
+        }
+        if(this.state.moedaOf == 'euro' && this.state.moedaTo == 'dolar'){
+            converted = valor * euro;
+            document.getElementById('resultado').innerHTML = converted.toFixed(2) + ' Dólar americano';
         }
     }
     
@@ -54,6 +76,7 @@ export default class Conversion extends Component {
                         <option></option>
                         <option type="text" value="dolar">Dolar</option>
                         <option type="text" value="real">Real</option>
+                        <option type="text" value="euro">Euro</option>
                     </select>
                     
                     <label> to </label>
@@ -62,6 +85,7 @@ export default class Conversion extends Component {
                         <option></option>
                         <option type="text" value="dolar">Dolar</option>
                         <option type="text" value="real">Real</option>
+                        <option type="text" value="euro">Euro</option>
                     </select>
 
                     <br/>
